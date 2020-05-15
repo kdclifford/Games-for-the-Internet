be09341ae7150f6393674999abc8a3313a8d2b14
kdclifford <KClifford1@uclan.ac.uk>
2020-05-15 17:41:48 +0100

    breakable block

---
+++ Games for the internet/Assets/BreakablePhysics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePhysics : MonoBehaviour
+{
+    public float torque;
+    public Vector2 direction;
+
+    private Rigidbody2D objectRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRigidbody = GetComponent<Rigidbody2D>();
+        objectRigidbody.AddForce(direction);
+        objectRigidbody.AddTorque(torque);
+    }
+
+   
+}
