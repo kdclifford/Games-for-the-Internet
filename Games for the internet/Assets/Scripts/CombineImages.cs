410640b68b8c7c8c0c9dd176cc30062d7a3245ae
kdclifford <KClifford1@uclan.ac.uk>
2020-05-17 23:49:54 +0100

    char custom

--- Games for the internet/Assets/Scripts/CombineImages.cs
+++ Games for the internet/Assets/Scripts/CombineImages.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class CombineImages : MonoBehaviour
 {
 
-    public Texture2D texture1;
-    public Texture2D texture2;
+    public Texture2D textureEye;
+    public Texture2D textureBody;
+    public Texture2D NewTexture;
+
+    public Color col1;
+    public Color col2;
 
     // Start is called before the first frame update
     void Start()
     {
-        texture1 = AddWatermark(texture1, texture2); 
+        NewTexture = MergeImage(textureBody, textureEye, NewTexture); 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 
 
     //public void CreateTexture(Texture2D texture, int width, int height, Color32 color)
@@ -35,28 +36,37 @@ public class CombineImages : MonoBehaviour
 
 
 
-    public Texture2D AddWatermark(Texture2D background, Texture2D watermark)
+    public Texture2D MergeImage(Texture2D Body, Texture2D Eyes, Texture2D NewText)
     {
-
+       
         int startX = 0;
-        int startY = background.height - watermark.height;
+        int startY = 0;
 
-        for (int x = startX; x < background.width; x++)
+        for (int x = startX; x < Body.width; x++)
         {
 
-            for (int y = startY; y < background.height; y++)
+            for (int y = startY; y < Body.height; y++)
             {
-                Color bgColor = background.GetPixel(x, y);
-                Color wmColor = watermark.GetPixel(x - startX, y - startY);
+                Color bgColor = Body.GetPixel(x, y);
+                Color wmColor = Eyes.GetPixel(x, y);
+                if(wmColor.a != 0)
+                {
+                    NewText.SetPixel(x, y, wmColor * col1);
+                }
+                else
+                {
+                    NewText.SetPixel(x, y, bgColor * col2);
+                }
 
-                Color final_color = Color.Lerp(bgColor, wmColor, wmColor.a / 1.0f);
 
-                background.SetPixel(x, y, final_color);
+
+                //Color final_color = Color.Lerp(bgColor, wmColor, wmColor.a / 1.0f);
+
             }
         }
 
-        background.Apply();
-        return background;
+        NewText.Apply();
+        return NewText;
     }
 
 }
